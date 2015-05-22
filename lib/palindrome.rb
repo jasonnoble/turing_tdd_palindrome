099a9f638b7e8c43f09931137a0da72d93fd954c
Jason Noble <github+jasonn@jasonnoble.org>
2015-05-22 15:52:06 -0600

    It works!

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,2 +1,16 @@
+# Comment goes here
 class Palindrome
+  def self.palindrome?(string)
+    # A string is a palindrome if:
+    #   it's length is < 2
+    #   or it's length is 2 and
+    #     the characters match
+    # string != 'ac'
+    return true if string.length < 2
+    letters = string.downcase.split('')
+    letters.select! { |letter| letter =~ /\w/ }
+    first_letter = letters.shift
+    last_letter = letters.pop
+    first_letter == last_letter && Palindrome.palindrome?(letters.join)
+  end
 end
--- spec/lib/palindrome_spec.rb
+++ spec/lib/palindrome_spec.rb
@@ -1,4 +1,64 @@
 require './lib/palindrome'
 
 describe Palindrome do
+  describe '#palindrome?(string)' do
+    it 'returns true for an empty string' do
+      expect(Palindrome.palindrome?('')).to eq(true)
+    end
+
+    it 'returns true for a single character string' do
+      expect(Palindrome.palindrome?('a')).to eq(true)
+    end
+
+    describe 'for two character strings' do
+      context 'where the two characters match' do
+        it 'returns true' do
+          expect(Palindrome.palindrome?('aa')).to eq(true)
+        end
+      end
+      context 'where the two characters do not match' do
+        it 'returns false' do
+          expect(Palindrome.palindrome?('ac')).to eq(false)
+        end
+      end
+    end
+
+    describe 'for three character strings' do
+      context 'when the first and last letter match' do
+        it 'returns true' do
+          expect(Palindrome.palindrome?('aba')).to eq(true)
+        end
+      end
+      context 'when the first and last letter do not match' do
+        it 'returns false' do
+          expect(Palindrome.palindrome?('ac')).to eq(false)
+        end
+      end
+    end
+    describe 'for four character strings' do
+      context 'when the first and last letter match' do
+        context 'when the inner first and last letter match' do
+          it 'returns true' do
+            expect(Palindrome.palindrome?('abba')).to eq(true)
+          end
+        end
+        context 'when the inner first and last letter no not match' do
+          it 'returns false' do
+            expect(Palindrome.palindrome?('abca')).to eq(false)
+          end
+        end
+        context 'when the first and last letter do not match' do
+          it 'returns false' do
+            expect(Palindrome.palindrome?('ac')).to eq(false)
+          end
+        end
+      end
+
+    end
+
+    it 'returns true for "A man, a plan, a canal, Panama!"' do
+      string = 'A man, a plan, a canal, Panama!'
+      expect(Palindrome.palindrome?(string)).to eq(true)
+    end
+  end
 end
